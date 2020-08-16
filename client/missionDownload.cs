a0a49b0f4fda470dcb4e08f9098e6a14bb554d79
Electrk <Electrk@ymail.com>
2020-08-16 10:06:03 -0700

    Cleaned up client/missionDownload.cs and added comments, most from TGE

--- client/missionDownload.cs
+++ client/missionDownload.cs
@@ -1,6 +1,29 @@
+// -----------------------------------------------------------------------------
+//  Torque Game Engine
+//  Copyright (C) GarageGames.com, Inc.
+// -----------------------------------------------------------------------------
+
+// -----------------------------------------------------------------------------
+//  Mission Loading
+//  Server download handshaking.  This produces a number of manifest, blob, and
+//  onPhaseX calls so the game scripts can update the game's GUI.
+//
+//  Loading Phases:
+//  Phase 0: Download File Manifest
+//  Phase 1: Download Datablocks
+//  Phase 2: Download Ghost Objects
+//  Phase 3: Scene Lighting
+// -----------------------------------------------------------------------------
+
+
+// ----------------------------------------------------------------------------
+//  Phase 0
+// ----------------------------------------------------------------------------
+
 function onManifestHashReceived ()
 {
-	$manifestPending = 1;
+	$manifestPending = true;
+
 	LoadingProgressTxt.setValue ("DOWNLOADING FILE MANIFEST");
 	LoadingProgress.setValue (0);
 	LoadingSecondaryProgress.setValue (0);
@@ -8,18 +31,18 @@ function onManifestHashReceived ()
 
 function onManifestRecieved ()
 {
-	$manifestPending = 0;
+	$manifestPending   = false;
 	$totalPendingBlobs = 0;
 }
 
-function setTotalPendingBlobs (%tpb)
+function setTotalPendingBlobs ( %tpb )
 {
 	$totalPendingBlobs = %tpb;
 }
 
 function onBlobCacheCheckFinished ()
 {
-	if ($totalPendingBlobs == 1)
+	if ( $totalPendingBlobs == 1 )
 	{
 		LoadingProgressTxt.setValue ("CHECKING CDN FOR 1 FILE");
 	}
@@ -27,28 +50,33 @@ function onBlobCacheCheckFinished ()
 	{
 		LoadingProgressTxt.setValue ("CHECKING CDN FOR " @ $totalPendingBlobs @ " FILES");
 	}
+
 	LoadingProgress.setValue (0);
 	LoadingSecondaryProgress.setValue (0);
 }
 
-function updateBlobsRemaining (%blobsRemaining)
+function updateBlobsRemaining ( %blobsRemaining )
 {
-	if ($manifestPending && %blobsRemaining == 1)
+	if ( $manifestPending && %blobsRemaining == 1 )
 	{
 		LoadingProgressTxt.setValue ("DOWNLOADING FILE MANIFEST");
 		LoadingProgress.setValue (0);
 		LoadingSecondaryProgress.setValue (0);
+
 		return;
 	}
-	if (%blobsRemaining > $totalPendingBlobs)
+
+	if ( %blobsRemaining > $totalPendingBlobs )
 	{
 		$totalPendingBlobs = %blobsRemaining;
 	}
-	if ($totalPendingBlobs > 0)
+
+	if ( $totalPendingBlobs > 0 )
 	{
 		LoadingProgressTxt.setValue ("DOWNLOADING FILE " @ ($totalPendingBlobs - %blobsRemaining) + 1 @ " OF " @ $totalPendingBlobs);
 		LoadingProgress.setValue (($totalPendingBlobs - %blobsRemaining) / $totalPendingBlobs);
 		LoadingSecondaryProgress.setValue (0);
+
 		Canvas.repaint ();
 	}
 }
@@ -58,58 +86,78 @@ function onBlobDownloadFinished ()
 	LoadingProgressTxt.setValue ("LOADING DATABLOCKS");
 	LoadingSecondaryProgress.setValue (0);
 	LoadingProgress.setValue (0);
+
 	commandToServer ('BlobDownloadFinished');
 }
 
-function setDownloadSize (%size)
+function setDownloadSize ( %size )
 {
 	$currDownloadSize = %size;
 }
 
-function updateDownloadProgress (%val)
+function updateDownloadProgress ( %val )
 {
 	%percent = %val / $currDownloadSize;
 	LoadingSecondaryProgress.setValue (%percent);
 }
 
-function clientCmdMissionStartPhase1 (%seq)
+// ----------------------------------------------------------------------------
+//  Phase 1
+// ----------------------------------------------------------------------------
+
+function clientCmdMissionStartPhase1 ( %seq )
 {
 	echo ("*** New Mission");
 	echo ("*** Phase 1: Download Datablocks & Targets");
+
 	onMissionDownloadPhase1 ();
+
 	commandToServer ('MissionStartPhase1Ack', %seq);
 }
 
-function onDataBlockObjectReceived (%index, %total)
+function onDataBlockObjectReceived ( %index, %total )
 {
 	onPhase1Progress (%index / %total);
 }
 
-function clientCmdMissionStartPhase2 (%seq)
+// ----------------------------------------------------------------------------
+//  Phase 2
+// ----------------------------------------------------------------------------
+
+function clientCmdMissionStartPhase2 ( %seq )
 {
 	onPhase1Complete ();
+
 	echo ("*** Phase 2: Download Ghost Objects");
+
 	purgeResources ();
+
 	onMissionDownloadPhase2 ();
 	commandToServer ('MissionStartPhase2Ack', %seq);
 }
 
-function onGhostAlwaysStarted (%ghostCount)
+function onGhostAlwaysStarted ( %ghostCount )
 {
-	$ghostCount = %ghostCount;
+	$ghostCount  = %ghostCount;
 	$ghostsRecvd = 0;
 }
 
 function onGhostAlwaysObjectReceived ()
 {
-	$ghostsRecvd += 1;
+	$ghostsRecvd++;
 	onPhase2Progress ($ghostsRecvd / $ghostCount);
 }
 
-function clientCmdMissionStartPhase3 (%seq)
+// ----------------------------------------------------------------------------
+//  Phase 3
+// ----------------------------------------------------------------------------
+
+function clientCmdMissionStartPhase3 ( %seq )
 {
 	onPhase2Complete ();
+
 	echo ("*** Phase 3: Mission Lighting");
+
 	$MSeq = %seq;
 	sceneLightingComplete ();
 }
@@ -117,7 +165,8 @@ function clientCmdMissionStartPhase3 (%seq)
 function updateLightingProgress ()
 {
 	onPhase3Progress ($SceneLighting::lightingProgress);
-	if ($lightingMission)
+
+	if ( $lightingMission )
 	{
 		$lightingProgressThread = schedule (1, 0, "updateLightingProgress");
 	}
@@ -126,9 +175,12 @@ function updateLightingProgress ()
 function sceneLightingComplete ()
 {
 	echo ("Mission lighting done");
+
 	onPhase3Complete ();
+
+	// This is also the end of the mission load cycle
 	onMissionDownloadComplete ();
+
 	commandToServer ('MissionStartPhase3Ack', $MSeq);
 	commandToServer ('RequestNamedTargets');
 }
-
