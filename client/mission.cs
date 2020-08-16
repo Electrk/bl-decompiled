b1865f20bb86b0d7a9b1023d64ca6388329f1e1a
Electrk <Electrk@ymail.com>
2020-08-16 09:56:25 -0700

    Cleaned up client/mission.cs and added comments from TGE 1.4

--- client/mission.cs
+++ client/mission.cs
@@ -1,12 +1,29 @@
-function clientCmdMissionStart (%seq)
+// -----------------------------------------------------------------------------
+//  Torque Game Engine
+//  Copyright (C) GarageGames.com, Inc.
+// -----------------------------------------------------------------------------
+
+// ----------------------------------------------------------------------------
+//  Mission start / end events sent from the server
+// ----------------------------------------------------------------------------
+
+
+function clientCmdMissionStart ( %seq )
 {
+	// The client receives a mission start right before
+	// being dropped into the game.
+
 	ClearPhysicsCache ();
 }
 
-function clientCmdMissionEnd (%seq)
+function clientCmdMissionEnd ( %seq )
 {
+	// Received when the current mission is ended.
 	alxStopAll ();
-	$lightingMission = 0;
-	$sceneLighting::terminateLighting = 1;
-}
 
+	// Disable mission lighting if it's going, this is here
+	// in case the mission ends while we are in the process
+	// of loading it.
+	$lightingMission = false;
+	$sceneLighting::terminateLighting = true;
+}
