e52fb4a57031260a014a7d012a8ac792e3083c32
Electrk <Electrk@ymail.com>
2020-08-16 09:36:21 -0700

    Cleaned up client/canvas.cs and added comments, mostly from TGE 1.4

--- client/canvas.cs
+++ client/canvas.cs
@@ -1,77 +1,109 @@
-function initCanvas (%windowName)
+// -----------------------------------------------------------------------------
+//  Torque Game Engine
+//  Copyright (C) GarageGames.com, Inc.
+// -----------------------------------------------------------------------------
+
+// -----------------------------------------------------------------------------
+//  Function to construct and initialize the default canvas window
+//  used by the games
+// -----------------------------------------------------------------------------
+
+
+function initCanvas ( %windowName )
 {
 	videoSetGammaCorrection ($pref::OpenGL::gammaCorrection);
-	if (!createCanvas (%windowName))
+
+	if ( !createCanvas (%windowName) )
 	{
 		quit ();
 		return;
 	}
+
 	setOpenGLTextureCompressionHint ($pref::OpenGL::compressionHint);
 	setOpenGLAnisotropy ($pref::OpenGL::anisotropy);
 	setOpenGLMipReduction ($pref::OpenGL::mipReduction);
 	setOpenGLSkyMipReduction ($pref::OpenGL::skyMipReduction);
+
+	// Init the audio system
 	OpenALInit ();
 }
 
 function resetCanvas ()
 {
 	echo ("Resetting Canvas...");
-	if (isObject (PlayGui))
+
+	if ( isObject (PlayGui) )
 	{
 		PlayGui.setHasRendered (0);
 	}
 }
 
+// Called when the game window comes back into focus
 function onWindowReactivate ()
 {
-	if ($windowReactivating)
+	if ( $windowReactivating )
 	{
 		return;
 	}
+
 	echo ("Window reactivating...");
-	$windowReactivating = 1;
-	if (isFullScreen () && isObject (Canvas))
+
+	$windowReactivating = true;
+
+	if ( isFullScreen () && isObject (Canvas) )
 	{
-		if (isObject (PlayGui))
+		if ( isObject (PlayGui) )
 		{
-			PlayGui.setHasRendered (0);
+			PlayGui.setHasRendered (false);
 		}
+
 		%oldShaderEnabled = $Shader::Enabled;
-		$Shader::Enabled = 0;
+		$Shader::Enabled  = false;
+
 		Canvas.repaint ();
+
 		flushTextureCache ();
 		regenerateShadowMapFBOs ();
+
 		Canvas.repaint ();
+
 		$Shader::Enabled = %oldShaderEnabled;
-		if ($Shader::Enabled)
+
+		if ( $Shader::Enabled )
 		{
 			initializeShaderAssets ();
 		}
 	}
-	$windowReactivating = 0;
+
+	$windowReactivating = false;
 }
 
+// Doesn't appear to be called by either the engine or other game scripts.
+// Unknown purpose.
 function restartAudio ()
 {
 	OpenALInit ();
-	if (!isObject (ServerConnection))
+
+	if ( !isObject (ServerConnection) )
 	{
 		return;
 	}
+
 	%group = ServerConnection.getId ();
 	%count = %group.getCount ();
-	%i = 0;
-	while (%i < %count)
+
+	for ( %i = 0; %i < %count; %i++ )
 	{
 		%obj = %group.getObject (%i);
-		if (%obj.getClassName () $= "AudioEmitter")
+
+		if ( %obj.getClassName () $= "AudioEmitter" )
 		{
 			%profile = %obj.profile;
+
 			%obj.profile = 0;
 			%obj.profile = %profile;
+
 			%obj.schedule (10, update);
 		}
-		%i += 1;
 	}
 }
-
