e9a571b728c4a39a72205489a1707ad09157f08e
Electrk <Electrk@ymail.com>
2020-08-16 09:47:33 -0700

    Cleaned up client/actionMap.cs

--- client/actionMap.cs
+++ client/actionMap.cs
@@ -1,33 +1,51 @@
-function ActionMap::copyBind (%this, %otherMap, %command)
+// -----------------------------------------------------------------------------
+//  Torque Game Engine
+//  Copyright (C) GarageGames.com, Inc.
+// -----------------------------------------------------------------------------
+
+// ------------------------------------------------------------------------------
+//  Utility remap functions:
+// ------------------------------------------------------------------------------
+
+
+function ActionMap::copyBind ( %this, %otherMap, %command )
 {
-	if (!isObject (%otherMap))
+	if ( !isObject (%otherMap) )
 	{
 		error ("ActionMap::copyBind - \"" @ %otherMap @ "\" is not an object!");
 		return;
 	}
+
 	%bind = %otherMap.getBinding (%command);
-	if (%bind !$= "")
+
+	if ( %bind !$= "" )
 	{
 		%device = getField (%bind, 0);
 		%action = getField (%bind, 1);
-		%flags = %otherMap.isInverted (%device, %action) ? "SDI" : "SD";
+
+		%flags    = %otherMap.isInverted (%device, %action) ? "SDI" : "SD";
 		%deadZone = %otherMap.getDeadZone (%device, %action);
-		%scale = %otherMap.getScale (%device, %action);
+		%scale    = %otherMap.getScale (%device, %action);
+
 		%this.bind (%device, %action, %flags, %deadZone, %scale, %command);
 	}
 }
 
-function ActionMap::blockBind (%this, %otherMap, %command)
+//------------------------------------------------------------------------------
+
+
+function ActionMap::blockBind ( %this, %otherMap, %command )
 {
-	if (!isObject (%otherMap))
+	if ( !isObject (%otherMap) )
 	{
 		error ("ActionMap::blockBind - \"" @ %otherMap @ "\" is not an object!");
 		return;
 	}
+
 	%bind = %otherMap.getBinding (%command);
-	if (%bind !$= "")
+
+	if ( %bind !$= "" )
 	{
 		%this.bind (getField (%bind, 0), getField (%bind, 1), "");
 	}
 }
-
